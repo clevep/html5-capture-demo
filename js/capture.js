8fc3002d7d2f9de0cf8593a741c3c9d155459565
Cleve Pasarell <clevepasarell@gmail.com>
2015-01-11 20:35:24 -0800

    Finish basic functionality

--- js/capture.js
+++ js/capture.js
@@ -1,10 +1,11 @@
 // Put event listeners into place
-window.addEventListener("DOMContentLoaded", function() {
+$(function() {
   // Grab elements, create settings, etc.
-  var canvas = document.getElementById("canvas"),
+  var canvas = $("canvas")[0],
     context = canvas.getContext("2d"),
-    video = document.getElementById("video"),
+    video = $("video")[0],
     videoObj = { "video": true },
+    videoStream,
     errBack = function(error) {
       console.log("Video capture error: ", error.code); 
     };
@@ -12,24 +13,38 @@ window.addEventListener("DOMContentLoaded", function() {
   // Put video listeners into place
   if(navigator.getUserMedia) { // Standard
     navigator.getUserMedia(videoObj, function(stream) {
+      videoStream = stream;
       video.src = stream;
       video.play();
     }, errBack);
   } else if(navigator.webkitGetUserMedia) { // WebKit-prefixed
     navigator.webkitGetUserMedia(videoObj, function(stream){
+      videoStream = stream;
       video.src = window.webkitURL.createObjectURL(stream);
       video.play();
     }, errBack);
   }
   else if(navigator.mozGetUserMedia) { // Firefox-prefixed
     navigator.mozGetUserMedia(videoObj, function(stream){
+      videoStream = stream;
       video.src = window.URL.createObjectURL(stream);
       video.play();
     }, errBack);
   }
 
-  document.getElementById("snap").addEventListener("click", function() {
+  $("#snap").on("click", function() {
     context.drawImage(video, 0, 0, 640, 480);
+    videoStream.stop();
+    var dataURL = canvas.toDataURL("image/jpeg"),
+      inputWrapper = $('.capture-input'),
+      outputWrapper = $('.capture-output'),
+      outputImg = outputWrapper.find('img'),
+      outputLink = outputWrapper.find('a');
+    outputImg.attr('src', dataURL);
+    outputLink.attr('href', dataURL);
+    outputLink.attr('download', 'capture-' + new Date().toISOString() + '.jpg');
+    inputWrapper.hide();
+    outputWrapper.show();
   });
 
-}, false);
+});
