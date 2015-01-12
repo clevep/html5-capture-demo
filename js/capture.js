b1936532a0ad7f4cc5c29a9b082e07c100ee9e96
Cleve Pasarell <clevepasarell@gmail.com>
2015-01-11 20:47:15 -0800

    Cleanup and comments.

--- js/capture.js
+++ js/capture.js
@@ -1,31 +1,31 @@
-// Put event listeners into place
 $(function() {
-  // Grab elements, create settings, etc.
   var canvas = $("canvas")[0],
     context = canvas.getContext("2d"),
     video = $("video")[0],
-    videoObj = { "video": true },
+    // basic video recording configuration for the getUserMedia API
+    videoConfig = { "video": true },
+    // cache our stream so we can stop it later
     videoStream,
     errBack = function(error) {
       console.log("Video capture error: ", error.code); 
     };
 
-  // Put video listeners into place
+  // put video listeners into place and start the stream
   if(navigator.getUserMedia) { // Standard
-    navigator.getUserMedia(videoObj, function(stream) {
+    navigator.getUserMedia(videoConfig, function(stream) {
       videoStream = stream;
       video.src = stream;
       video.play();
     }, errBack);
   } else if(navigator.webkitGetUserMedia) { // WebKit-prefixed
-    navigator.webkitGetUserMedia(videoObj, function(stream){
+    navigator.webkitGetUserMedia(videoConfig, function(stream){
       videoStream = stream;
       video.src = window.webkitURL.createObjectURL(stream);
       video.play();
     }, errBack);
   }
   else if(navigator.mozGetUserMedia) { // Firefox-prefixed
-    navigator.mozGetUserMedia(videoObj, function(stream){
+    navigator.mozGetUserMedia(videoConfig, function(stream){
       videoStream = stream;
       video.src = window.URL.createObjectURL(stream);
       video.play();
@@ -33,16 +33,25 @@ $(function() {
   }
 
   $("#snap").on("click", function() {
+    // capture a frame to the canvas and stop the stream
     context.drawImage(video, 0, 0, 640, 480);
     videoStream.stop();
+
+    // convert canvas image to dataURL
     var dataURL = canvas.toDataURL("image/jpeg"),
+
+    // lookup DOM elements
       inputWrapper = $('.capture-input'),
       outputWrapper = $('.capture-output'),
       outputImg = outputWrapper.find('img'),
       outputLink = outputWrapper.find('a');
+
+    // setup output
     outputImg.attr('src', dataURL);
     outputLink.attr('href', dataURL);
     outputLink.attr('download', 'capture-' + new Date().toISOString() + '.jpg');
+
+    // show output
     inputWrapper.hide();
     outputWrapper.show();
   });
